553b5fae7f6e120b2ea9b7e367c79d80d8907657
ST <stoo891@gmail.com>
2024-06-29 15:44:20 +0900

    環境構築

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            ['over_name' => '田中',
+            'under_name' => '太郎',
+            'over_name_kana' => 'タナカ',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 'tt@mail.com',
+            'sex' => '1',
+            'birth_day' => '2000-01-01',
+            'role' => '1',
+            'password' => 'password'],
+        ]);
 
     }
 }
